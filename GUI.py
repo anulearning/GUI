98886a627597da5a96c26f0170c55cdbbe993dbf
anuradha <anulearning3@gmail.com>
2021-10-16 14:12:23 -0400

    test commit from pycharm

---
+++ GUI.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+# Creates a blank window with minimize,maximize n close. in this case root is empty window.
+root = Tk()
+# keeps the window on the computer screen until closed
+root.mainloop()
